85470470c53d0736d2869ce5a6d929bc2d99f70e
AdrienneMota <adriennemota97@gmail.com>
2022-12-11 20:46:29 -0400

    feat: customers/ post

--- src/serve.js
+++ src/serve.js
@@ -22,6 +22,13 @@ const gameSchema = Joi.object({
     pricePerDay: Joi.number().greater(0).required()
 })
 
+const customerSchema = Joi.object({
+    name: Joi.string().min(1).required(),
+    phone: Joi.string().min(10).max(11),
+    cpf: Joi.string().min(11).required(),
+    birthday: Joi.string().min(1).required()    
+})
+
 //conexão com o banco   
 const connectiondb = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -115,6 +122,39 @@ app.get('/games', async(req, res) => {
         console.log(error)
         res.sendStatus(500)
     }
+})
+
+//clientes
+
+app.post('/customers', async (req, res) => {
+    const customer = req.body
+
+    try {
+        const { error } = customerSchema.validate(customer, {abortEarly: false})
+        if(error){
+            const erros = error.details.map((detail) => detail.message)
+            return res.status(400).send(erros)
+        }
+
+        const cpfExist = await connectiondb.query("SELECT * FROM customers WHERE cpf = $1;", [customer.cpf])
+        if(cpfExist.rows.length > 0){
+            return res.status(400).send({message: 'O cpf do cliente informado já existe.'})
+        }
+
+
+        const {name, phone, cpf, birthday} = customer
+
+        await connectiondb.query(
+            'INSERT INTO customers ("name", "phone", "cpf", "birthday") VALUES ($1, $2, $3, $4);', 
+            [name, phone, cpf, birthday]
+            )
+
+        res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+
 })
 
 const port = process.env.PORT || 4000
